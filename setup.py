c6d2fab5105f513bbbec92a9cd92dd35c243c134
Luan Guimarães <luang@protonmail.ch>
2018-05-24 17:33:48 -0300

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gabby',
+    version='0.0.1',
+    description='A simple controller for message queue pipelines',
+    url='http://github.com/luanguimaraesla/gabby',
+    author='Luan Guimarães Lacerda',
+    license='MIT',
+    packages=['gabby'],
+    zip_safe=False,
+)
