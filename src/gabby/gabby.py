dde53fdc49b09e728b9db00c427c275985600137
Luan Guimarães <luang@protonmail.ch>
2018-05-25 04:07:35 -0300

    Add Gabby module

--- src/gabby/gabby.py
+++ src/gabby/gabby.py
@@ -2,8 +2,54 @@
 Gabby module witch creates the Gabby class to handle creation of
 message queue nodes for intercommunication
 """
+from collections import namedtuple
 
-class Gabby:
-    """
-    Gabby is a class to create each node instance of the system.
-    """
+from .mqtt.transmitter import Transmitter, Receiver
+from .messager.decoder import decode
+from .messager.message import Message
+
+Topic = namedtuple('Topic', ['name', 'topic', 'fmt'])
+
+class Gabby(Transmitter, Receiver):
+    def __init__(self, input_topics, output_topics, decode_input=True,
+                 encode_output=True, url=None, port=None, keepalive=None):
+        """
+        Processor initializer
+
+        Args:
+            topics (collection):
+                output topics to send results
+
+            decoder (bool), default=True:
+                enable auto encoding/decoding of any received message
+        """
+        Receiver.__init__(self, input_topics, url, port, keepalive)
+        Transmitter.__init__(self, name, output_topics, url, port, keepalive)
+        self.decode_input = decode_input
+        self.encode_output = encode_output
+
+    def process(self, userdata, message):
+        if self.decode_input:
+            topic_name = message.topic
+            topic = filter(lambda x: x.name == topic_name, self.input_topics)
+            message = decode(message, topic.fmt)
+
+        response_messages = self.process(message)
+
+        self.send(self.process(message), self.encode_output)
+
+    def transform(self, message):
+        """
+        Abstract method to process any received message
+
+        Args:
+            message (Message or paho.mqtt.MQTTMessage):
+                message from queue decoded or not depending on 'decode' var
+
+            author (str):
+                message's writer
+
+        Return:
+            Collection of messages to be transmitted or an empty list
+        """
+        return [Message(message.data)]
